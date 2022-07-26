0b68bf5e5652fdffd208efebb1ed9daea9f9423c
HAL9000\wmsau <william.sauve@decenternet.net>
2022-07-26 17:37:29 +0900

    Starting to work on spawners + physics.

---
+++ Assets/Components/Objects/EnemySpawner.re.ts
@@ -0,0 +1,31 @@
+import * as RE from 'rogue-engine';
+import Spawner from './Spawner.re';
+
+export default class EnemySpawner extends Spawner {
+
+
+  private _spawnCooldown: number = 3;
+  private _counter: number = 0;
+
+  awake() {
+
+  }
+
+  start() {
+ 
+  }
+
+  update() {
+    this._counter += RE.Runtime.deltaTime;
+
+    if(this._counter >= this._spawnCooldown){
+      let _newSphere = this._spawned.instantiate();
+      _newSphere.position.x = 20;
+      _newSphere.position.y = 200;
+      this._counter = 0;
+      RE.Debug.log("Spawning a new thing.");
+    }
+  }
+}
+
+RE.registerComponent(EnemySpawner);
---
+++ Assets/Components/Objects/Spawner.re.ts
@@ -0,0 +1,9 @@
+import * as RE from 'rogue-engine';
+import SceneObject from './SceneObject.re';
+
+export default class Spawner extends SceneObject {
+
+  @RE.props.prefab() _spawned: RE.Prefab;
+}
+
+RE.registerComponent(Spawner);
